1. use tf.log1p tf.expm1 2. add batch_size to get_input_sequences 3. change dilation parameter in function temporal_convolution 4. change some typos 5. correct the calculation of pad_len in temportal_convlution 6. convert to python3

cnn.py
@@ -26,12 +26,16 @@ class DataReader(object):
         ]
         data = [np.load(os.path.join(data_dir, '{}.npy'.format(i))) for i in data_cols]
 
+        # for experiment
+        for k in range(len(data)):
+            data[k] = data[k][:10000]
+
         self.test_df = DataFrame(columns=data_cols, data=data)
         self.train_df, self.val_df = self.test_df.train_test_split(train_size=0.95)
 
-        print 'train size', len(self.train_df)
-        print 'val size', len(self.val_df)
-        print 'test size', len(self.test_df)
+        print(('train size', len(self.train_df)))
+        print(('val size', len(self.val_df)))
+        print(('test size', len(self.test_df)))
 
     def train_batch_generator(self, batch_size):
         return self.batch_generator(
@@ -120,29 +124,31 @@ class cnn(TFBaseModel):
         self.num_decode_steps = num_decode_steps
         super(cnn, self).__init__(**kwargs)
 
+    # subtract mean
     def transform(self, x):
-        return tf.log(x + 1) - tf.expand_dims(self.log_x_encode_mean, 1)
+        return tf.log1p(x) - tf.expand_dims(self.log_x_encode_mean, 1)
 
     def inverse_transform(self, x):
-        return tf.exp(x + tf.expand_dims(self.log_x_encode_mean, 1)) - 1
+        #return tf.exp(x + tf.expand_dims(self.log_x_encode_mean, 1)) - 1
+        return tf.expm1(x + tf.expand_dims(self.log_x_encode_mean, 1))
 
     def get_input_sequences(self):
-        self.x_encode = tf.placeholder(tf.float32, [None, None])
-        self.encode_len = tf.placeholder(tf.int32, [None])
-        self.y_decode = tf.placeholder(tf.float32, [None, self.num_decode_steps])
-        self.decode_len = tf.placeholder(tf.int32, [None])
-        self.is_nan_encode = tf.placeholder(tf.float32, [None, None])
-        self.is_nan_decode = tf.placeholder(tf.float32, [None, self.num_decode_steps])
+        self.x_encode = tf.placeholder(tf.float32, [self.batch_size, None])
+        self.encode_len = tf.placeholder(tf.int32, [self.batch_size])
+        self.y_decode = tf.placeholder(tf.float32, [self.batch_size, self.num_decode_steps])
+        self.decode_len = tf.placeholder(tf.int32, [self.batch_size])
+        self.is_nan_encode = tf.placeholder(tf.float32, [self.batch_size, None])
+        self.is_nan_decode = tf.placeholder(tf.float32, [self.batch_size, self.num_decode_steps])
 
-        self.page_id = tf.placeholder(tf.int32, [None])
-        self.project = tf.placeholder(tf.int32, [None])
-        self.access = tf.placeholder(tf.int32, [None])
-        self.agent = tf.placeholder(tf.int32, [None])
+        self.page_id = tf.placeholder(tf.int32, [self.batch_size])
+        self.project = tf.placeholder(tf.int32, [self.batch_size])
+        self.access = tf.placeholder(tf.int32, [self.batch_size])
+        self.agent = tf.placeholder(tf.int32, [self.batch_size])
 
         self.keep_prob = tf.placeholder(tf.float32)
         self.is_training = tf.placeholder(tf.bool)
 
-        self.log_x_encode_mean = sequence_mean(tf.log(self.x_encode + 1), self.encode_len)
+        self.log_x_encode_mean = sequence_mean(tf.log1p(self.x_encode), self.encode_len) #length is batch length
         self.log_x_encode = self.transform(self.x_encode)
         self.x = tf.expand_dims(self.log_x_encode, 2)
 
@@ -184,7 +190,7 @@ class cnn(TFBaseModel):
                 output_units=2*self.residual_channels,
                 convolution_width=filter_width,
                 causal=True,
-                dilation_rate=[dilation],
+                dilation_rate=dilation,
                 scope='dilated-conv-encode-{}'.format(i)
             )
             conv_filter, conv_gate = tf.split(dilated_conv, 2, axis=2)
@@ -225,7 +231,7 @@ class cnn(TFBaseModel):
                 output_units=2*self.residual_channels,
                 convolution_width=filter_width,
                 causal=True,
-                dilation_rate=[dilation],
+                dilation_rate=dilation,
                 scope='dilated-conv-decode-{}'.format(i)
             )
             conv_filter, conv_gate = tf.split(dilated_conv, 2, axis=2)
@@ -248,7 +254,8 @@ class cnn(TFBaseModel):
         return y_hat
 
     def decode(self, x, conv_inputs, features):
-        batch_size = tf.shape(x)[0]
+        #batch_size = tf.shape(x)[0]
+        batch_size = self.batch_size
 
         # initialize state tensor arrays
         state_queues = []
@@ -297,15 +304,15 @@ class cnn(TFBaseModel):
 
                 state = queue.read(time)
                 with tf.variable_scope('dilated-conv-decode-{}'.format(i), reuse=True):
-                    w_conv = tf.get_variable('weights'.format(i))
-                    b_conv = tf.get_variable('biases'.format(i))
+                    w_conv = tf.get_variable('weights')
+                    b_conv = tf.get_variable('biases')
                     dilated_conv = tf.matmul(state, w_conv[0, :, :]) + tf.matmul(x_proj, w_conv[1, :, :]) + b_conv
                 conv_filter, conv_gate = tf.split(dilated_conv, 2, axis=1)
                 dilated_conv = tf.nn.tanh(conv_filter)*tf.nn.sigmoid(conv_gate)
 
                 with tf.variable_scope('dilated-conv-proj-decode-{}'.format(i), reuse=True):
-                    w_proj = tf.get_variable('weights'.format(i))
-                    b_proj = tf.get_variable('biases'.format(i))
+                    w_proj = tf.get_variable('weights')
+                    b_proj = tf.get_variable('biases')
                     concat_outputs = tf.matmul(dilated_conv, w_proj) + b_proj
                 skips, residuals = tf.split(concat_outputs, [self.skip_channels, self.residual_channels], axis=1)
 
@@ -393,7 +400,7 @@ if __name__ == '__main__':
         prediction_dir=os.path.join(base_dir, 'predictions'),
         optimizer='adam',
         learning_rate=.001,
-        batch_size=128,
+        batch_size=12,
         num_training_steps=200000,
         early_stopping_steps=5000,
         warm_start_init_step=0,
@@ -411,6 +418,8 @@ if __name__ == '__main__':
         filter_widths=[2 for i in range(8)]*3,
         num_decode_steps=64,
     )
+
+
     nn.fit()
     nn.restore()
     nn.predict()

data_frame.py
@@ -27,14 +27,14 @@ class DataFrame(object):
         self.length = lengths[0]
         self.columns = columns
         self.data = data
-        self.dict = dict(zip(self.columns, self.data))
+        self.dict = dict(list(zip(self.columns, self.data)))
         self.idx = np.arange(self.length)
 
     def shapes(self):
-        return pd.Series(dict(zip(self.columns, [mat.shape for mat in self.data])))
+        return pd.Series(dict(list(zip(self.columns, [mat.shape for mat in self.data]))))
 
     def dtypes(self):
-        return pd.Series(dict(zip(self.columns, [mat.dtype for mat in self.data])))
+        return pd.Series(dict(list(zip(self.columns, [mat.dtype for mat in self.data]))))
 
     def shuffle(self):
         np.random.shuffle(self.idx)
@@ -67,7 +67,7 @@ class DataFrame(object):
         return DataFrame(copy.copy(self.columns), [mat[mask] for mat in self.data])
 
     def __iter__(self):
-        return self.dict.items().__iter__()
+        return list(self.dict.items()).__iter__()
 
     def __len__(self):
         return self.length
@@ -77,7 +77,7 @@ class DataFrame(object):
             return self.dict[key]
 
         elif isinstance(key, int):
-            return pd.Series(dict(zip(self.columns, [mat[self.idx[key]] for mat in self.data])))
+            return pd.Series(dict(list(zip(self.columns, [mat[self.idx[key]] for mat in self.data]))))
 
     def __setitem__(self, key, value):
         assert value.shape[0] == len(self), 'matrix first dimension does not match'

tf_base_model.py
@@ -8,6 +8,7 @@ import numpy as np
 import tensorflow as tf
 
 from tf_utils import shape
+import imp
 
 
 class TFBaseModel(object):
@@ -98,13 +99,17 @@ class TFBaseModel(object):
 
         self.graph = self.build_graph()
         self.session = tf.Session(graph=self.graph)
-        print 'built graph'
+        print('built graph')
 
     def calculate_loss(self):
         raise NotImplementedError('subclass must implement this')
 
     def fit(self):
         with self.session.as_default():
+
+            from tensorflow.python import debug as tf_debug
+            #self.session = tf_debug.LocalCLIDebugWrapperSession(self.session)
+            #self.session.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
 
             if self.warm_start_init_step:
                 self.restore(self.warm_start_init_step)
@@ -125,7 +130,7 @@ class TFBaseModel(object):
             while step < self.num_training_steps:
 
                 # validation evaluation
-                val_batch_df = val_generator.next()
+                val_batch_df = next(val_generator)
                 val_feed_dict = {
                     getattr(self, placeholder_name, None): data
                     for placeholder_name, data in val_batch_df if hasattr(self, placeholder_name)
@@ -144,18 +149,18 @@ class TFBaseModel(object):
                 val_loss_history.append(val_loss)
 
                 if hasattr(self, 'monitor_tensors'):
-                    for name, tensor in self.monitor_tensors.items():
+                    for name, tensor in list(self.monitor_tensors.items()):
                         [np_val] = self.session.run([tensor], feed_dict=val_feed_dict)
-                        print name
-                        print 'min', np_val.min()
-                        print 'max', np_val.max()
-                        print 'mean', np_val.mean()
-                        print 'std', np_val.std()
-                        print 'nans', np.isnan(np_val).sum()
-                        print
+                        print(name)
+                        print('min', np_val.min())
+                        print('max', np_val.max())
+                        print('mean', np_val.mean())
+                        print('std', np_val.std())
+                        print('nans', np.isnan(np_val).sum())
+                        print()
 
                 # train step
-                train_batch_df = train_generator.next()
+                train_batch_df = next(train_generator)
                 train_feed_dict = {
                     getattr(self, placeholder_name, None): data
                     for placeholder_name, data in train_batch_df if hasattr(self, placeholder_name)
@@ -234,7 +239,7 @@ class TFBaseModel(object):
                 if hasattr(self, 'is_training'):
                     test_feed_dict.update({self.is_training: False})
 
-                tensor_names, tf_tensors = zip(*self.prediction_tensors.items())
+                tensor_names, tf_tensors = list(zip(*list(self.prediction_tensors.items())))
                 np_tensors = self.session.run(
                     fetches=tf_tensors,
                     feed_dict=test_feed_dict
@@ -242,14 +247,14 @@ class TFBaseModel(object):
                 for tensor_name, tensor in zip(tensor_names, np_tensors):
                     prediction_dict[tensor_name].append(tensor)
 
-            for tensor_name, tensor in prediction_dict.items():
+            for tensor_name, tensor in list(prediction_dict.items()):
                 np_tensor = np.concatenate(tensor, 0)
                 save_file = os.path.join(self.prediction_dir, '{}.npy'.format(tensor_name))
                 logging.info('saving {} with shape {} to {}'.format(tensor_name, np_tensor.shape, save_file))
                 np.save(save_file, np_tensor)
 
         if hasattr(self, 'parameter_tensors'):
-            for tensor_name, tensor in self.parameter_tensors.items():
+            for tensor_name, tensor in list(self.parameter_tensors.items()):
                 np_tensor = tensor.eval(self.session)
 
                 save_file = os.path.join(self.prediction_dir, '{}.npy'.format(tensor_name))
@@ -288,7 +293,7 @@ class TFBaseModel(object):
         date_str = datetime.now().strftime('%Y-%m-%d_%H-%M')
         log_file = 'log_{}.txt'.format(date_str)
 
-        reload(logging)  # bad
+        imp.reload(logging)  # bad
         logging.basicConfig(
             filename=os.path.join(log_dir, log_file),
             level=logging.INFO,

tf_utils.py
@@ -19,8 +19,10 @@ def temporal_convolution_layer(inputs, output_units, convolution_width, causal=F
     """
     with tf.variable_scope(scope, reuse=reuse):
         if causal:
-            shift = (convolution_width / 2) + (int(dilation_rate[0] - 1) / 2)
-            pad = tf.zeros([tf.shape(inputs)[0], shift, inputs.shape.as_list()[2]])
+            #shift = (convolution_width // 2) + (int(dilation_rate[0] - 1) // 2)
+            pad_len = dilation_rate * (convolution_width - 1)
+            print('kernel_size = {}, dilation = {}, shift = {}'.format(convolution_width, dilation_rate, pad_len))
+            pad = tf.zeros([tf.shape(inputs)[0], pad_len, inputs.shape.as_list()[2]])
             inputs = tf.concat([pad, inputs], axis=1)
 
         W = tf.get_variable(
@@ -32,7 +34,7 @@ def temporal_convolution_layer(inputs, output_units, convolution_width, causal=F
             shape=[convolution_width, shape(inputs, 2), output_units]
         )
 
-        z = tf.nn.convolution(inputs, W, padding='SAME', dilation_rate=dilation_rate)
+        z = tf.nn.convolution(inputs, W, padding='SAME', dilation_rate=[dilation_rate])
         if bias:
             b = tf.get_variable(
                 name='biases',
@@ -40,9 +42,18 @@ def temporal_convolution_layer(inputs, output_units, convolution_width, causal=F
                 shape=[output_units]
             )
             z = z + b
-        z = activation(z) if activation else z
-        z = tf.nn.dropout(z, dropout) if dropout is not None else z
-        z = z[:, :-shift, :] if causal else z
+
+        if activation:
+            z = activation(z)
+
+        if dropout and dropout > 0:
+            z = tf.nn.dropout(z, dropout)
+
+        if causal:
+            z = z[:, :-pad_len, :]
+        #z = activation(z) if activation else z
+        #z = tf.nn.dropout(z, dropout) if dropout is not None else z
+        #z = z[:, :-shift, :] if causal else z
         return z
 
 
@@ -80,8 +91,13 @@ def time_distributed_dense_layer(inputs, output_units, bias=True, activation=Non
         if batch_norm is not None:
             z = tf.layers.batch_normalization(z, training=batch_norm, reuse=reuse)
 
-        z = activation(z) if activation else z
-        z = tf.nn.dropout(z, dropout) if dropout is not None else z
+        if activation:
+            z = activation(z)
+
+        if dropout and dropout > 0:
+            z = tf.nn.dropout(z, dropout)
+        #z = activation(z) if activation else z
+        #z = tf.nn.dropout(z, dropout) if dropout is not None else z
         return z
 
 
@@ -91,12 +107,13 @@ def shape(tensor, dim=None):
         return tensor.shape.as_list()
     else:
         return tensor.shape.as_list()[dim]
+        #return tensor.shape[dim].__int__()
 
 
 def sequence_smape(y, y_hat, sequence_lengths, is_nan):
     max_sequence_length = tf.shape(y)[1]
     y = tf.cast(y, tf.float32)
-    smape = 2*(tf.abs(y_hat - y) / (tf.abs(y) + tf.abs(y_hat)))
+    smape = 200*(tf.abs(y_hat - y) / (tf.abs(y) + tf.abs(y_hat)))
 
     # ignore discontinuity
     zero_loss = 2.0*tf.ones_like(smape)